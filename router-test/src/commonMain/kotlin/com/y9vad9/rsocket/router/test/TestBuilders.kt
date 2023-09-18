7413cd22fadf0ecbf35abe9e23b1ad9acc1e0d41
y9vad9 <y9vad9@gmail.com>
2023-09-18 19:57:25 +0200

    Optimized emptyRSocket builder

--- router-test/src/commonMain/kotlin/com/y9vad9/rsocket/router/test/TestBuilders.kt
+++ router-test/src/commonMain/kotlin/com/y9vad9/rsocket/router/test/TestBuilders.kt
@@ -1,13 +1,12 @@
 package com.y9vad9.rsocket.router.test
 
-import com.y9vad9.rsocket.router.Router
-import com.y9vad9.rsocket.router.annotations.InternalRouterApi
-import com.y9vad9.rsocket.router.builders.RouterBuilder
 import io.rsocket.kotlin.RSocket
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
-public fun emptyRSocket(): RSocket = object : RSocket {
+public object EmptyRSocket : RSocket {
     override val coroutineContext: CoroutineContext
         get() = EmptyCoroutineContext
 }
+
+public fun emptyRSocket(): RSocket = EmptyRSocket
